393f7b9bf2b648cc3558c52fc5b64507424bfaca
al448322 <al448322@uji.es>
2025-10-08 10:52:54 +0200

    Ejercicio 2 realizado (No pasa el test)

--- Practica1.java
+++ Practica1.java
@@ -19,7 +19,7 @@ public class Practica1 {
         for (int i = 0; i < numeros.size(); i++) {
             int a = numeros.get(i);
             for (int j = 0; j < numeros.size(); j++) {
-                if (i != j) {
+                if (i != j) { //Compruebo si NO es el mismo indice, ya que comparar el mismo numero consigo mismo seria siempre 0
                     int b = numeros.get(j);
                     if (a % b == 0) {
                         resultado.add(a);
@@ -33,7 +33,16 @@ public class Practica1 {
 
     //EJERCICIO2
     public static void separate(Set<Integer> cuadrados, Set<Integer> noCuadrados) {
-        //TODO
+        Set<Integer> union = new HashSet<>(cuadrados);
+        union.addAll(noCuadrados);  //Uno ambos Set en 1 solo para poder iterarlo luego
+
+        Set<Integer> nCuadrados = new HashSet<>();
+        for (Integer n : union) {
+            nCuadrados.add(n * n);  //Guardo n^n en el nuevo set
+        }
+
+        cuadrados.retainAll(nCuadrados);    //
+        noCuadrados.removeAll(nCuadrados);
 
     }
 
